144c723aface6e518077a1a7b49bf612f06ac33f
DEV.HYUCK <wwh3218@gmail.com>
2026-01-30 19:29:56 +0900

    feat : Spring 의존성 추가 및 운영 관련

---
+++ src/main/java/com/example/nbcamprojcet/NbcamProjcetApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbcamprojcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbcamProjcetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NbcamProjcetApplication.class, args);
+    }
+
+}
